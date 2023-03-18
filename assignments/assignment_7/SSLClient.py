d31658470c0502ff71db5b7c89b782cfaddeb9c9
Helga Amelia Bergland <helgag13@uia.no>
2023-03-18 16:48:23 +0100

    SSLClient-edit

--- assignments/assignment_7/SSLClient.py
+++ assignments/assignment_7/SSLClient.py
@@ -1,37 +1,40 @@
 import socket
 import ssl
-
-HOST = "localhost"
-PORT = 8443
-CERT_FILE = "example.crt"
+import sys
+import random
+import string
 
 def main():
-    context = ssl.create_default_context(cafile=CERT_FILE, capath=".")
-    context.check_hostname = False
-    context.verify_mode = ssl.CERT_NONE
+    # The server IP address and port should be the local loopback interface localhost and the
+    # port to contact the same as selected in Task 2.
+    server_ip = "localhost"
+    server_port = 8443
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
-        conn = context.wrap_socket(sock, server_side=False, server_hostname=HOST)
-        conn.connect((HOST, PORT))
+    # Create a socket object
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        print("Cipher suite:", conn.cipher()[0])
+    # Load the certificate in the file example.crt
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH, cafile="example.crt")
+    context.load_verify_locations(capath=".")
 
-        while True:
-            message = input("Enter a message to send (type 'exit' to quit): ")
-            if message.lower() == "exit":
-                break
+    # Wrap the socket with the SSL context
+    ssl_sock = context.wrap_socket(sock, server_hostname=server_ip)
 
-            conn.sendall(message.encode("utf-8"))
-            data = conn.recv(1024)
-            
-            if not data:
-                print("No data received.")
-                break
-            
-            print("Received from server:", data.decode("utf-8"))
-        
-        # Close the SSL connection
-        conn.close()
+    # Connect to the server
+    ssl_sock.connect((server_ip, server_port))
+
+    # Print the negotiated cipher suite
+    print("Negotiated Cipher Suite: ", ssl_sock.cipher())
+
+    # Test communication: send a lowercase sentence, receive it in uppercase
+    test_data = "".join([random.choice(string.ascii_lowercase) for i in range(15)]) + "\n"
+    ssl_sock.sendall(test_data.encode("utf-8"))
+
+    response = ssl_sock.recv(1024)
+    print("Received from server: ", response.decode("utf-8"))
+
+    # Close the SSL socket
+    ssl_sock.close()
 
 if __name__ == "__main__":
     main()
