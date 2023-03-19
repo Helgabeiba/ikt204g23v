6ecdf670558ebb103e7bb1c1480c0f4678a6e477
Helga Amelia Bergland <helgag13@uia.no>
2023-03-19 17:28:00 +0100

    test#1000000

--- assignments/assignment_4/UDPClient.py
+++ assignments/assignment_4/UDPClient.py
@@ -2,7 +2,7 @@ from socket import *
 serverName = 'localhost'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-strMessage = input('Input lowercase sentence')
+strMessage = input('Input lowercase sentence: ')
 byteMessage = bytearray(strMessage, 'utf-8')
 clientSocket.sendto(byteMessage, (serverName, serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
