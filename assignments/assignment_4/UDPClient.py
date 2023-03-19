2a461734a4bff61e31906dbec2f98c9bd54b057a
Helga Amelia Bergland <helgag13@uia.no>
2023-03-19 13:45:18 +0100

    small changes

--- assignments/assignment_4/UDPClient.py
+++ assignments/assignment_4/UDPClient.py
@@ -2,7 +2,7 @@ from socket import *
 serverName = 'localhost'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = input('Input lowercase sentence')
+message = input('testing')
 clientSocket.sendto(message.encode(),(serverName, serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 print(modifiedMessage.decode())
