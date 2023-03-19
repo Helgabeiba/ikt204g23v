6eaa3a7542cc319a7d2c2f9fe6f4658dc7390675
Helga Amelia Bergland <helgag13@uia.no>
2023-03-19 14:10:51 +0100

    test2

--- assignments/assignment_4/UDPClient.py
+++ assignments/assignment_4/UDPClient.py
@@ -1,8 +1,8 @@
 from socket import *
 serverName = 'localhost'
-serverPort = 12000
+serverPort = 12008
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = input('testing')
+message = input('Input lowercase sentence')
 clientSocket.sendto(message.encode(),(serverName, serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 print(modifiedMessage.decode())
--- assignments/assignment_4/UDPServer.py
+++ assignments/assignment_4/UDPServer.py
@@ -1,6 +1,6 @@
 from socket import *
-serverPort = 12000
 serverName = 'localhost'
+serverPort = 12008
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
 print('The server is ready to receive')
