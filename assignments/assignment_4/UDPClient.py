5876241fd8f50d86dc4304a3148fde09a8cb9bf3
Helga Amelia Bergland <helgag13@uia.no>
2023-03-19 14:49:03 +0100

    added bytemsg

--- assignments/assignment_4/UDPClient.py
+++ assignments/assignment_4/UDPClient.py
@@ -1,9 +1,10 @@
 from socket import *
 serverName = 'localhost'
-serverPort = 12008
+serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = input('Input lowercase sentence')
-clientSocket.sendto(message.encode(),(serverName, serverPort))
+strMessage = input('Input lowercase sentence')
+byteMessage = bytearray(strMessage, 'utf-8')
+clientSocket.sendto(byteMessage,(serverName, serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print(modifiedMessage.decode())
+print(modifiedMessage.decode('utf-8'))
 clientSocket.close()
